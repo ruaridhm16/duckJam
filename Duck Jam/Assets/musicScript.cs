fceba59e40e26b6f86c8ca57c487b43f8d280c27
ruaridhm16 <66776319+ruaridhm16@users.noreply.github.com>
2023-02-25 12:21:11 +0000

    test 2

--- Duck Jam/Assets/musicScript.cs
+++ Duck Jam/Assets/musicScript.cs
@@ -6,7 +6,7 @@ public class musicScript : MonoBehaviour
 {
     public duckCounter duckCount;
     public int level;
-    public bool played = false;
+ 
 
     [SerializeField] private AudioSource level1Layer1;
     [SerializeField] private AudioSource level1Layer2;
@@ -19,59 +19,74 @@ public class musicScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log(level1Layer1.time);
-        Debug.Log(level1Layer2.time);
-        Debug.Log(level1Layer3.time);
-        Debug.Log(level1Layer4.time);
-        Debug.Log(level1Layer5.time);
-
         if (level == 1)
         {
-            if (duckCounter.duckCount == 1 && !plays[0])
-            {
-                level1Layer1.Play(0);
-                plays[0] = true;
-            }
-            if (duckCounter.duckCount == 2 && !plays[1])
-            {
-                level1Layer2.time = level1Layer1.time;
-                level1Layer2.Play(0);
-                plays[1] = true;
-            }
-            if (duckCounter.duckCount == 3 && !plays[2])
-            {
-                level1Layer3.time = level1Layer1.time;
-                level1Layer3.Play(0);
-                plays[2] = true;
-            }
-            if (duckCounter.duckCount == 4 && !plays[3])
-            {
-                level1Layer4.time = level1Layer1.time;
-                level1Layer4.Play(0);
-                plays[3] = true;
-            }
-            if (duckCounter.duckCount == 5 && !plays[4])
-            {
-                level1Layer5.time = level1Layer1.time;
-                level1Layer5.Play(0);
-                plays[4] = true;
-            }
-          
-            else if (duckCounter.duckCount == 0)
-            {
-                level1Layer1.Stop();
-                level1Layer2.Stop();
-                level1Layer3.Stop();
-                level1Layer4.Stop();
-                level1Layer5.Stop();
-            }
+            level1Layer1.Play(0);
+            level1Layer1.volume = 0;
+            level1Layer2.Play(0);
+            level1Layer2.volume = 0;
+            level1Layer3.Play(0);
+            level1Layer3.volume = 0;
+            level1Layer4.Play(0);
+            level1Layer4.volume = 0;
+            level1Layer5.Play(0);
+            level1Layer5.volume = 0;
         }
-        
     }
-}
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log(level1Layer1.time);
+            Debug.Log(level1Layer2.time);
+            Debug.Log(level1Layer3.time);
+            Debug.Log(level1Layer4.time);
+            Debug.Log(level1Layer5.time);
+
+            if (level == 1)
+            {
+
+                if (duckCounter.duckCount == 1 && !plays[0])
+                {
+                    level1Layer1.volume = 100;
+                    plays[0] = true;
+                }
+                if (duckCounter.duckCount == 2 && !plays[1])
+                {
+                    level1Layer2.volume = 100;
+                    plays[1] = true;
+                }
+                if (duckCounter.duckCount == 3 && !plays[2])
+                {
+                    level1Layer3.volume = 100;
+                    plays[2] = true;
+                }
+                if (duckCounter.duckCount == 4 && !plays[3])
+                {
+                    level1Layer4.volume = 50;
+                    plays[3] = true;
+                }
+                if (duckCounter.duckCount == 5 && !plays[4])
+                {
+                    level1Layer5.volume = 100;
+                    plays[4] = true;
+                }
+
+                else if (duckCounter.duckCount == 0)
+                {
+
+                    level1Layer1.volume = 0;
+
+                    level1Layer2.volume = 0;
+
+                    level1Layer3.volume = 0;
+
+                    level1Layer4.volume = 0;
+
+                    level1Layer5.volume = 0;
+                }
+            }
+
+        }
+    }
+
